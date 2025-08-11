a931f6e643a97d258d46c874c8c7b0f30e763fd3
manlikexavy <chibokaxavier@gmail.com>
2025-08-11 18:21:14 +0100

    first commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="flex justify-between items-center h-[80px] px-10 bg-[#E6EAEE]">
+      <Image src="/Logo.svg" height={100} width={100} alt="logo" />
+      <div>
+        <div className="size-[50px] rounded-full bg-[#0CBC8B] flex items-center justify-center text-[16px] font-medium text-white">GA</div>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
---
+++ components/Sidebar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='w-[263px]'>Sidebar</div>
+  )
+}
+
+export default Sidebar
