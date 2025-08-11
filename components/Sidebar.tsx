ac6f53ea78628cccfc684427895102407d7a8ebe
manlikexavy <chibokaxavier@gmail.com>
2025-08-11 19:16:58 +0100

    installed lucide icons, working on side bar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+
+const links = [
+  {
+    icon: "/globe.svg",
+    name: "Get Started",
+  },
+  {
+    icon: "/element-1.svg",
+    name: "Dashboard",
+  },
+  {
+    icon: "/empty-wallet.svg",
+    name: "Accounts",
+  },
+  {
+    icon: "/coins-swap.svg",
+    name: "Transfers",
+  },
+  {
+    icon: "/document.svg",
+    name: "Transactions",
+  },
+  {
+    icon: "/setting-2.svg",
+    name: "Settings",
+  },
+];
 
 const Sidebar = () => {
   return (
-    <div className='w-[263px] border-gray-300 border border-r-[1px]'>Sidebar</div>
-  )
-}
+    <div className="w-[263px] border-gray-300 border border-r-[1px] flex flex-col pl-10">
+      {links.map((link, i) => (
+        <div className="flex items-center justify-center h-[52px]">
+          <Image
+            src={link.icon}
+            height={24}
+            width={24}
+            alt="bell"
+            className="text-white"
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
