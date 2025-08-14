a78900ebf7c5493a3566d69756f84e38d11ba200
manlikexavy <chibokaxavier@gmail.com>
2025-08-14 19:57:55 +0100

    added middleware

---
+++ middleware.ts
@@ -0,0 +1,20 @@
+// middleware.ts
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+
+  // Redirect from root to /dashboard
+  if (pathname === "/") {
+    const url = req.nextUrl.clone();
+    url.pathname = "/dashboard";
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/"], // Only run middleware for the root path
+};
